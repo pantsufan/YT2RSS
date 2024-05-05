13f63a6812e5f152c69bbb35e455289650d6cc02
Joel Rodrigues <45329484+pantsufan@users.noreply.github.com>
2024-05-05 11:36:15 +0530

    refactor: fixes #1

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import json
 import os
 
 # Open the JSON file for reading
-with open('newpipe-subscriptions.json', 'r') as file:
+with open('newpipe-subscriptions.json', 'r', encoding='utf-8') as file:
     # Load the data into a variable
     subscriptions = json.load(file)
 
@@ -15,7 +15,7 @@ for sub in subscriptions['subscriptions']:
     new_url = url.replace('https://www.youtube.com/channel/', 'https://www.youtube.com/feeds/videos.xml?channel_id=')
 
     # Write the URL to a new text file
-    with open('urls.txt', 'a') as outfile:
+    with open('urls.txt', 'a', encoding='utf-8') as outfile:
         outfile.write(f'{new_url}\n')
 
 
@@ -25,18 +25,18 @@ for sub in subscriptions['subscriptions']:
     name = sub['name']
 
     # Write the URL to a new text file
-    with open('names.txt', 'a') as outfile:
+    with open('names.txt', 'a', encoding='utf-8') as outfile:
         outfile.write(f'{name}\n')
 
 # Creates OPML file
 def create_opml_file(urls_file, names_file, output_file):
-    with open(urls_file, 'r') as urls_file:
+    with open(urls_file, 'r', encoding='utf-8') as urls_file:
         feed_urls = [line.strip() for line in urls_file.readlines()]
 
-    with open(names_file, 'r') as names_file:
+    with open(names_file, 'r', encoding='utf-8') as names_file:
         website_names = [line.strip() for line in names_file.readlines()]
 
-    with open(output_file, 'w') as opml_file:
+    with open(output_file, 'w', encoding='utf-8') as opml_file:
         opml_file.write('<?xml version="1.0" encoding="UTF-8"?>\n')
         opml_file.write('<opml version="1.0">\n')
         opml_file.write('<head>\n')
